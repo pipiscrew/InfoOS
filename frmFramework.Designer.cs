0758c745302b0ba73e5f52796e2bf3ea807427f9
pipiscrew <pipiscrew@gmail.com>
2021-09-11 22:37:10 +0300

    add framework detection

---
+++ frmFramework.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace InfoOS
+{
+    partial class frmFramework
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstv = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.ctx = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctx.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstv
+            // 
+            this.lstv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colSP});
+            this.lstv.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lstv.FullRowSelect = true;
+            this.lstv.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstv.Location = new System.Drawing.Point(13, 13);
+            this.lstv.Name = "lstv";
+            this.lstv.Size = new System.Drawing.Size(856, 411);
+            this.lstv.TabIndex = 2;
+            this.lstv.UseCompatibleStateImageBehavior = false;
+            this.lstv.View = System.Windows.Forms.View.Details;
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 200;
+            // 
+            // colSP
+            // 
+            this.colSP.Text = "Service Pack";
+            this.colSP.Width = 100;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(828, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Location = new System.Drawing.Point(13, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(695, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "only installed frameworks listed.  source : HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft" +
+    "\\NET Framework Setup\\NDP";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(827, 437);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(42, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "source";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // ctx
+            // 
+            this.ctx.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripCopy});
+            this.ctx.Name = "ctx";
+            this.ctx.Size = new System.Drawing.Size(153, 48);
+            // 
+            // toolStripCopy
+            // 
+            this.toolStripCopy.Name = "toolStripCopy";
+            this.toolStripCopy.Size = new System.Drawing.Size(152, 22);
+            this.toolStripCopy.Text = "copy";
+            this.toolStripCopy.Click += new System.EventHandler(this.toolStripCopy_Click);
+            // 
+            // frmFramework
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 464);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstv);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmFramework";
+            this.Text = "frmFramework";
+            this.ctx.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstv;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader colSP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ContextMenuStrip ctx;
+        private System.Windows.Forms.ToolStripMenuItem toolStripCopy;
+    }
+}
---
+++ frmFramework.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoOS
+{
+    public partial class frmFramework : Form
+    {
+        //https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+        // for the history - https://github.com/justin0522/FrameworkDetect
+
+        public frmFramework()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstv.Items.Clear();
+            GetFrmRegistry();
+        }
+
+        internal void GetFrmRegistry()
+        {  // https://github.com/jmalarcon/DotNetVersions
+            using (RegistryKey ndpKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (!string.IsNullOrEmpty(install))
+                        {
+                            if (!(string.IsNullOrEmpty(sp)) && install == "1")
+                            {
+                                AddItem(name, sp);
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (!string.IsNullOrEmpty(name))
+                                sp = subKey.GetValue("SP", "").ToString();
+
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (!string.IsNullOrEmpty(install))
+                            {
+                                if (!(string.IsNullOrEmpty(sp)) && install == "1")
+                                {
+                                    AddItem(name + string.Format(" ({0})", subKeyName), sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    AddItem(name + string.Format(" ({0})", subKeyName));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        internal void AddItem(string col1, string col23 = "-")
+        {
+            ListViewItem l = new ListViewItem("v" + col1);
+            l.SubItems.Add(col23);
+
+            lstv.Items.Add(l);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/jmalarcon/DotNetVersions");
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                 ctx.Show(System.Windows.Forms.Cursor.Position);
+            }
+        }
+
+        private void toolStripCopy_Click(object sender, EventArgs e)
+        {
+            General.Copy2Clipboard(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP");
+        }
+    }
+}
